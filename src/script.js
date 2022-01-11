642946e9cf7b78b07dcd5192a018776d438cfe84
lhwei456 <lhwei.winni@gmail.com>
2022-01-11 22:04:42 +0800

    add two light and controller

--- src/script.js
+++ src/script.js
@@ -46,12 +46,50 @@ scene.add(pointLight)
 
 // 2
 const pointLight2 = new THREE.PointLight(0xff0000, 2)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
-pointLight2.position.set(1, 1, 1)
+pointLight2.position.set(-1, 51, -1, 64)
 pointLight2.intensity = 1
 scene.add(pointLight2)
+
+const light1 = gui.addFolder('Light 1')
+
+// GUI light controller
+light1.add(pointLight2.position, 'x').min(-6).max(6).step(.01)
+light1.add(pointLight2.position, 'y').min(-3).max(3).step(.01)
+light1.add(pointLight2.position, 'z').min(-3).max(3).step(.01)
+light1.add(pointLight2, 'intensity').min(0).max(10).step(.01)
+
+// light helper
+const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
+scene.add(pointLightHelper)
+
+// 3
+const pointLight3 = new THREE.PointLight(0xff0000, 2)
+pointLight3.position.set(-1, 51, -1, -1.64)
+pointLight3.intensity = 10
+scene.add(pointLight3)
+
+const light2 = gui.addFolder('Light 2')
+
+// GUI light controller
+light2.add(pointLight3.position, 'x').min(-6).max(6).step(.01)
+light2.add(pointLight3.position, 'y').min(-3).max(3).step(.01)
+light2.add(pointLight3.position, 'z').min(-3).max(3).step(.01)
+light2.add(pointLight3, 'intensity').min(0).max(10).step(.01)
+
+const light2Color = {
+    color: 0xff0000
+}
+
+light2.addColor(light2Color, 'color')
+.onChange(() => {
+    pointLight3.color.set(light2Color.color)
+})
+
+// light helper
+const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, 1)
+scene.add(pointLightHelper3)
+
+
 
 /**
  * Sizes
